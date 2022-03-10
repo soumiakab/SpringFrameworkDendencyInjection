2c203f6f5f24b3618ed7bd0079fa1194e84d641d
KABRANESOUMIA <kabrane.soumia.com>
2022-03-10 10:29:31 +0100

    add geometry example

--- geometry/src/main/java/app/Main.java
+++ geometry/src/main/java/app/Main.java
@@ -9,13 +9,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //ClassPathXmlApplicationContext charge la définition du contexte à partir d'un fichier XML contenu dans le classpath.
         ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+
+       // La méthode getBean() de l'interface BeanFactory qui attend en paramètre l'identifiant de l'objet permet d'obtenir une instance de cet objet selon la configuration fournie au conteneur.
+
 
         GeometryExample1 geometryBean1=context.getBean("geometryExample1", GeometryExample1.class);
         geometryBean1.drawShape();
-
+/*
         GeometryExample2 geometryBean2 = context.getBean("geometryExample2", GeometryExample2.class);
         geometryBean2.drawShape();
-
+*/
     }
 }
